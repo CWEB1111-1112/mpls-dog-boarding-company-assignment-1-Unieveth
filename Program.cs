8bb6d5b9c3c67ae969a340ffc0746429794d2c26
Unieveth <git@unieveth.com>
2021-02-04 13:27:04 -0600

    project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assingment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
